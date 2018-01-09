7b18774db7e07183b6ffad40a222371dcf28da0a
cwoodlock <g00341460@gmit.ie>
2018-01-09 18:58:11 +0000

    First Commit, made runner class

---
+++ DocumentSimilarity/src/ie/gmit/sw/Runner.java
@@ -0,0 +1,18 @@
+package ie.gmit.sw;
+
+/*
+ * Colm Woodlock
+ * G00341460
+ * Object Oriented Programming Main Assignment
+ */
+
+public class Runner {
+	public static void main(String[] args) {
+		//Create an instance of the UI(user interface)
+		UI userInterface = new UI();
+		
+		userInterface.mainMenu();
+	}
+	
+
+}
