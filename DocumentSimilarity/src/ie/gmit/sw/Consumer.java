d687f2fa612d6cd4651f59efd10ec70399dc47bc
cwoodlock <g00341460@gmit.ie>
2018-01-11 21:17:45 +0000

    Implemented consumer method

--- DocumentSimilarity/src/ie/gmit/sw/Consumer.java
+++ DocumentSimilarity/src/ie/gmit/sw/Consumer.java
@@ -4,6 +4,9 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.ExecutorCompletionService;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 /*
  * Colm Woodlock
@@ -15,11 +18,21 @@ public class Consumer implements Runnable{
 	
 	//Variables
 	private BlockingQueue<Shingle> q;
-	private int k;
+	private int k;	//minhashes
 	private int [] hashes;	//the random stuff
 	private Map<Integer, List<Integer>> map = new HashMap<>();
+	private ExecutorService pool;
 	
 	public Consumer(BlockingQueue<Shingle> q, int k, int poolSize) {
+		this.q = q;
+		this.k =k;
+		pool = Executors.newFixedThreadPool(poolSize);
+		init();
+		
+	}
+
+	private void init() {
+		
 		
 	}
 
